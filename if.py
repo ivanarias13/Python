89395b3835db36ad0bc668b9a61a40d38fffd6ce
ivanarias13 <ivanarias13@gmail.com>
2020-01-13 15:54:52 -0600

    lecciones

---
+++ if.py
@@ -0,0 +1,10 @@
+#include "python.h"
+
+
+name = "ivan"
+if len(name) >=6:
+    print("Name is long")
+elif len(name) == 4:
+    print("name is 4 char")
+elif len(name) >= 3:
+    print("name is short")
