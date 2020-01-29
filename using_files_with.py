1c432fdddeecb3d64de2405652b6291234ed763e
ivanarias13 <ivanarias13@gmail.com>
2020-01-29 11:54:22 -0600

    using files

---
+++ using_files_with.py
@@ -0,0 +1,15 @@
+with open('xmen.txt', 'w+') as my_file:
+    my_file = open('xmen.txt', 'w+')
+    my_file.write('Beast\n')
+    my_file.write('Phoenix\n')
+    my_file.writelines([
+         'Cyclops\n',
+        'Bishop\n',
+        'NightCrawler\n',
+])
+my_file = open('xmen.txt', 'r')
+with my_file:
+    print(my_file.read())
+    print("I'm reading again")
+    my_file.seek(0)
+    print(my_file.read())
